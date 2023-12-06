e83038f1c9374ad644edcae02ea768ec2742a7ad
Musat Leonard <musatleonard@yahoo.com>
2023-12-06 08:32:12 +0200

    handle exception

--- Lab_6/ex2.py
+++ Lab_6/ex2.py
@@ -2,22 +2,26 @@ import os
 import sys
 
 def change_name(directory_path):
-    if not os.path.isdir(directory_path):
-        raise FileNotFoundError("Directory not found:", directory_path)
-    count = 1
+    try:
+        if not os.path.isdir(directory_path):
+            raise FileNotFoundError("Directory not found:", directory_path)
+        count = 1
 
-    for filename in os.listdir(directory_path):
-        old_path = os.path.join(directory_path, filename)
-        string = "file"
-        string = string + str(count)
-        new_path = os.path.join(directory_path, string)
+        for filename in os.listdir(directory_path):
+                old_path = os.path.join(directory_path, filename)
+                string = "file"
+                string = string + str(count)
+                new_path = os.path.join(directory_path, string)
 
-        if os.path.exists(directory_path + filename):
-            raise Exception ("File already exists.")
+                if os.path.exists(directory_path + filename):
+                    raise Exception ("File already exists.")
 
-        os.rename(old_path, new_path)
-        count += 1
-
+                os.rename(old_path, new_path)
+                count += 1
+    except FileNotFoundError as e:
+        print("Error:", e)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
 
 
 if len(sys.argv) != 2:
--- Lab_6/ex3.py
+++ Lab_6/ex3.py
@@ -3,18 +3,23 @@ import sys
 
 def sum_size(directory_path):
     sum = 0
-    if not os.path.isdir(directory_path):
-        raise FileNotFoundError("Directory not found:", directory_path)
+    try:
+        if not os.path.isdir(directory_path):
+            raise FileNotFoundError("Directory not found:", directory_path)
 
-    for filename in os.listdir(directory_path):
-            file_path = os.path.join(directory_path, filename)
+        for filename in os.listdir(directory_path):
+                file_path = os.path.join(directory_path, filename)
 
-            if os.path.isdir(file_path):
-                sum =sum + sum_size(file_path)
-            else:
-                sum = sum + os.path.getsize(file_path)
-
+                if os.path.isdir(file_path):
+                    sum =sum + sum_size(file_path)
+                else:
+                    sum = sum + os.path.getsize(file_path)
+    except FileNotFoundError as e:
+        print("Error:", e)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
     return sum
+
 
 if len(sys.argv) != 2:
     print("Usage: python script.py <directory_path> <file_extension>")
